3d23ca6bb9064c413b59faab36a870d613138d16
Andrii Melnichuk <andrewlutsk88@gmail.com>
2016-11-10 18:34:06 +0200

    make map and button hover color

--- resources/views/FrontEnd/contact.blade.php
+++ resources/views/FrontEnd/contact.blade.php
@@ -23,33 +23,7 @@
     {{-- start: Map --}}
     <div class="map">
 
-        {{-- starts: Google Maps --}}
-        <div id="googlemaps-container-top"></div>
-        <div id="googlemaps" class="google-map google-map-full"></div>
-        <div id="googlemaps-container-bottom"></div>
-
-        <script src="{{ asset('/js/frontend/jquery-2.0.3.min.js') }}"></script>
-        <script src="http://maps.google.com/maps/api/js?sensor=true&key=AIzaSyARimfCVH19VyYWgXVxeIBJ9LUGdels4E4"></script>
-        <script src="{{ asset('/js/frontend/jquery.gmap.min.js') }}"></script>
-
-        <script type="text/javascript">
-
-            $('#googlemaps').gMap({
-                maptype: 'ROADMAP',
-                scrollwheel: true,
-                zoom: 16,
-                markers: [
-                    {
-                        address: "{{ $texts->get('header.coordinates') }}", // Your Adress Here
-                        html: '',
-                        popup: false,
-                    }
-
-                ],
-
-            });
-        </script>
-        {{-- end: Google Maps --}}
+        {!! $texts->get('header.map') !!}
 
     </div>
     {{-- end: Map --}}
